124461f944358831a7d6c028f61b95509de201d5
Alex B <bugaev.alexander.k@gmail.com>
2013-09-28 22:03:01 +0300

    New Rails 4 app with Ruby 2.0 on board

---
+++ even2cal/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Even2cal::Application.config.secret_key_base = 'ca5c9dbc51ad8e54082b9175d38d0ab178734ceae7d67ed991a14d136b819fd0fd469d81caeed548646908a318a364c98b6762ace7f8bb8a40c7161f446cbcbb'
