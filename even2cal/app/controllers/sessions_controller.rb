c0cb89d762f7ccf6391ac616b83ab4682b97b3f4
Alex B <bugaev.alexander.k@gmail.com>
2013-10-26 01:28:12 +0300

    Moving main logic from controller into module

--- even2cal/app/controllers/sessions_controller.rb
+++ even2cal/app/controllers/sessions_controller.rb
@@ -1,22 +1,21 @@
 class SessionsController < ApplicationController
-  require 'google/api_client'
+  require 'social_network_connector'
   require 'json'
-  require 'date'
 
   def index
     @vk_authorized = vk_authorized
     @google_authorized = google_authorized
-    @calendars = get_calendar_list(session[:google][:token]) if @google_authorized
-		@event_pairs = JSON.parse(session[:vkontakte][:events]).reverse.to_a if @google_authorized &&
-                                                                            @vk_authorized &&
-                                                                            session[:google][:calendar_id]
+    @calendars = set_calendars if @google_authorized
+    if @google_authorized && @vk_authorized && session[:google][:calendar_id]
+		  @event_pairs = JSON.parse(session[:vkontakte][:events]).reverse.to_a 
+    end
     @activePage = flash[:page] ? flash[:page] : 0
   end
 
   def vk_auth
     session[:vkontakte] = {
       token: auth_hash['credentials']['token'],
-      events: get_vk_user_events(auth_hash['credentials']['token'])
+      events: set_vk_session_events 
     }
     flash[:page] = 2
     flash[:success] = "Вы успешно авторизовались в Вконтакте" 
@@ -38,7 +37,7 @@ class SessionsController < ApplicationController
 
   def import_events
     if vk_authorized
-			import_events_to_calendar(session[:google][:calendar_id], params[:selected_events])
+      do_import
 	    flash[:success] = "Выбранные события были импортированы! А теперь проверьте свой календарь." 
 			redirect_to root_path		
 		else
@@ -60,96 +59,20 @@ class SessionsController < ApplicationController
 
   protected
 
-  def import_events_to_calendar(calendar_id, selected_events_array)
-    client = Google::APIClient.new(
-      :application_name => "Even2Cal",
-      :application_version => "0.1")
-    client.authorization.access_token = session[:google][:token]
-    service = client.discovered_api('calendar', 'v3')  
-
-    vk_events = JSON.parse(session[:vkontakte][:events]).select do |event|
-      selected_events_array.include?(event["gid"].to_s)
-		end
-
-    vk_events.each do |event|
-      client.execute(
-        api_method: service.events.insert,
-        parameters: {'calendarId' => calendar_id},
-        body: prepare_params_from_event(event),
-        headers: {'Content-Type' => 'application/json'}
-      )
-    end
+  def do_import
+    SocialNetworkConnector::Googler.import_events_to_calendar(session[:google][:calendar_id],
+                                                              params[:selected_events],
+                                                              session[:google][:token],
+                                                              session[:vkontakte][:token],
+                                                              session[:vkontakte][:events])
   end
 
-  def prepare_params_from_event(event)
-		# TIMEZONE STILL HARDCODED
-		# CHECK more easy way of time converting
-    result = {
-      'summary' => event["name"],
-      'description' => ActionView::Base.full_sanitizer.sanitize(event["description"]),
-      'start' => {
-        'dateTime' => DateTime.parse(Time.at(event["start_date"].to_i).to_s).rfc3339,
-        'timeZone' => 'Europe/Minsk' 
-      },
-      'end' => {
-        'dateTime' => DateTime.parse(Time.at(event["start_date"].to_i + 1800).to_s).rfc3339,
-        'timeZone' => 'Europe/Minsk'
-      },
-      'location' => get_event_location(event)
-    }
-    
-    JSON.dump(result)
+  def set_vk_session_events
+    SocialNetworkConnector::Vkontakte.get_vk_user_events(auth_hash['credentials']['token'])
   end
 
-  def get_event_location(event)
-		if event["place"]
-      @vk = VkontakteApi::Client.new(session[:vkontakte][:token])
-      country = @vk.places.getCountryById(:cids => [event["place"]["country"]]).first["name"] if event["place"]["country"]
-      city = @vk.places.getCityById(:cids => [event["place"]["city"]]).first["name"] if event["place"]["city"]
-      address = event["place"]["address"] if event["place"]["address"]
-      "#{address}, #{city}, #{country}"
-		else
-		  ""
-		end
-  end
-
-  def get_calendar_list(auth_token)
-    client = Google::APIClient.new(
-      :application_name => "Even2Cal",
-      :application_version => "0.1")
-    client.authorization.access_token = auth_token
-
-    service = client.discovered_api('calendar', 'v3')
-
-    calendar_list = client.execute(
-      :api_method => service.calendar_list.list,
-      :parameters => {},
-      :headers => {'Content-Type' => 'application/json'})
-    calendar_list.data.items.collect {|cal| [cal.summary, cal.id]}
-  end
-
-  def get_vk_user_events(auth_token)
-#   DO NOT FORGET TO REMOVE COUNTER LIMITS
-    @vk = VkontakteApi::Client.new(auth_token)
-    group_fields = ['place', 'description', 'start_date', 'end_date']
-
-    all_groups = @vk.groups.get(extended: 1,
-                                  fields: group_fields,
-															    count: 30)
-    all_groups.shift
-    publics = @vk.groups.get(extended: 1,
-                               filter: ['publics'],
-                               fields: group_fields,
-														   count: 10)
-    publics.shift
-    simple_groups = @vk.groups.get(extended: 1,
-                                     filter: ['groups'],
-                                     fields: group_fields,
-																		 count: 10)
-    simple_groups.shift
-    
-    events = all_groups - publics - simple_groups
-    events.select! {|event| Time.at(event["start_date"].to_i) > Time.now}.to_json
+  def set_calendars
+    SocialNetworkConnector::Googler.get_calendar_list(session[:google][:token])
   end
 
   def auth_hash
---
+++ even2cal/lib/social_network_connector.rb
@@ -0,0 +1,109 @@
+module SocialNetworkConnector
+  class Googler
+    require 'google/api_client'
+    require 'json'
+    require 'date'
+
+    class << self
+      def get_calendar_list(auth_token)
+        client = Google::APIClient.new(
+          :application_name => "Even2Cal",
+          :application_version => "0.1")
+        client.authorization.access_token = auth_token
+    
+        service = client.discovered_api('calendar', 'v3')
+    
+        calendar_list = client.execute(
+          :api_method => service.calendar_list.list,
+          :parameters => {},
+          :headers => {'Content-Type' => 'application/json'})
+        calendar_list.data.items.collect {|cal| [cal.summary, cal.id]}
+      end
+
+      def import_events_to_calendar(calendar_id, selected_events_array, google_token, vk_token, events)
+        client = Google::APIClient.new(
+          :application_name => "Even2Cal",
+          :application_version => "0.1")
+        client.authorization.access_token = google_token 
+        service = client.discovered_api('calendar', 'v3')  
+    
+        vk_events = JSON.parse(events).select do |event|
+          selected_events_array.include?(event["gid"].to_s)
+    		end
+    
+        vk_events.each do |event|
+          client.execute(
+            api_method: service.events.insert,
+            parameters: {'calendarId' => calendar_id},
+                  body: prepare_params_from_event(event, vk_token), 
+               headers: {'Content-Type' => 'application/json'}
+          )
+        end
+      end
+
+      protected
+
+      def prepare_params_from_event(event, vk_token)
+       	# TIMEZONE STILL HARDCODED
+       	# CHECK more easy way of time converting
+         result = {
+           'summary' => event["name"],
+           'description' => ActionView::Base.full_sanitizer.sanitize(event["description"]),
+           'start' => {
+             'dateTime' => DateTime.parse(Time.at(event["start_date"].to_i).to_s).rfc3339,
+             'timeZone' => 'Europe/Minsk' 
+           },
+           'end' => {
+             'dateTime' => DateTime.parse(Time.at(event["start_date"].to_i + 1800).to_s).rfc3339,
+             'timeZone' => 'Europe/Minsk'
+           },
+           'location' => SocialNetworkConnector::Vkontakte.get_event_location(event, vk_token)
+         }
+         
+         JSON.dump(result)
+       end
+    end
+  end
+
+  class Vkontakte
+    class << self
+      def get_vk_user_events(auth_token)
+#       DO NOT FORGET TO REMOVE COUNTER LIMITS
+        vk = VkontakteApi::Client.new(auth_token)
+        group_fields = ['place', 'description', 'start_date', 'end_date']
+
+        all_groups = vk.groups.get(extended: 1,
+                                     fields: group_fields,
+      													      count: 30)
+        all_groups.shift
+        publics = vk.groups.get(extended: 1,
+                                  filter: ['publics'],
+                                  fields: group_fields,
+      												     count: 10)
+        publics.shift
+        simple_groups = vk.groups.get(extended: 1,
+                                        filter: ['groups'],
+                                        fields: group_fields,
+      														       count: 10)
+        simple_groups.shift
+        
+#       VK has a bug in API - it's cannot just return groups with type 'event' 
+        events = all_groups - publics - simple_groups
+#       Add user Timezone here
+        events.select! {|event| Time.at(event["start_date"].to_i) > Time.now}.to_json
+      end
+
+      def get_event_location(event, vk_token)
+	      if event["place"]
+          vk = VkontakteApi::Client.new(vk_token)
+          country = vk.places.getCountryById(:cids => [event["place"]["country"]]).first["name"] if event["place"]["country"]
+          city = vk.places.getCityById(:cids => [event["place"]["city"]]).first["name"] if event["place"]["city"]
+          address = event["place"]["address"] if event["place"]["address"]
+          "#{address}, #{city}, #{country}"
+	    	else
+	    	  ""
+	    	end
+      end
+    end
+  end
+end
