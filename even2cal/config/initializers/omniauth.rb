5446609c732f7c06109118a87c2370407c0ad57c
Alex B <bugaev.alexander.k@gmail.com>
2013-10-30 01:43:30 +0300

    Additional scope for GoogleAPI was added: fix for production error

--- even2cal/config/initializers/omniauth.rb
+++ even2cal/config/initializers/omniauth.rb
@@ -2,5 +2,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :vkontakte, ENV['API_KEY'], ENV['API_SECRET'],
     :scope => 'groups', :display => 'page'
   provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"],
-    :scope => 'calendar', :prompt => 'consent', :name => 'google'  
+    :scope => ['calendar', 'userinfo.profile', 'userinfo.email'], :prompt => 'consent', :name => 'google'  
+
+  OmniAuth.config.on_failure = Proc.new { |env|
+    OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+  }
 end
+
